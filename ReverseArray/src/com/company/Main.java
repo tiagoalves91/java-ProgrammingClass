05c4dd110fb992d1abd21867477f2e3e68ded34b
tiagoalves91 <tiagorafael.goncalves@gmail.com>
2021-03-25 16:51:55 +0000

    arrays

---
+++ ReverseArray/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array = {5,12,15,20,30};
+
+        System.out.println(Arrays.toString(array));
+
+        reverse(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void reverse (int [] array) {
+
+        int maxIndex = array.length-1;
+        int halfLength = array.length /2;
+
+        for (int i =0; i<halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex -i];
+            array[maxIndex -i] = temp;
+
+        }
+    }
+}
